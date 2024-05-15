1063a19151d22e2e1e2425f23e347c1d173494f4
Ajay sharma <aakirtsharma123@gmail.com>
2024-05-15 19:32:04 +0545

    first commit

---
+++ dir/index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let connectedPlayers = 0;
+const MAX_PLAYERS = 2;
+
+io.on("connection", (socket) => {
+  if (connectedPlayers < MAX_PLAYERS) {
+    connectedPlayers++;
+
+    socket.on("reset", () => {
+      io.emit("resetGame");
+    });
+    socket.on("playerMove", (index) => {
+      io.emit("updateGame", index);
+    });
+
+    socket.on("disconnect", () => {
+      connectedPlayers--;
+    });
+  } else {
+    socket.emit("maxPlayersReached");
+    socket.disconnect(true);
+  }
+});
+
+app.use(express.static("./"));
+app.get("/", (req, res) => {
+  return res.sendFile("index.html");
+});
+server.listen(9000, () => console.log("server stated"));
